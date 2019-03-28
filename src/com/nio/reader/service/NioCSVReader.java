4c1ad10e764c75babad98561b8819451cec15b39
coolaider <chloroformus@gmail.com>
2019-03-28 03:10:28 +0300

    cleanup

--- src/com/nio/reader/service/NioCSVReader.java
+++ src/com/nio/reader/service/NioCSVReader.java
@@ -5,12 +5,9 @@ import com.nio.reader.model.Product;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -37,23 +34,17 @@ public class NioCSVReader {
     }
 
 
-
-
     private void runWorkers(ConcurrentLinkedDeque<FileWorker> workers, int concurrentConsumers) throws InterruptedException {
         //fixme: more flexible and better way to do this is use zip() operator, but seems like no way to hug request() backpressure with zipped observables. here we have hard components coupling
         if (!workers.isEmpty()) {
             List<FileWorker> runnedWorkersPool = new ArrayList<>();
             IntStream.range(0, workers.size() >= concurrentConsumers ? workers.size() : concurrentConsumers).forEach(x -> Optional.ofNullable(workers.poll()).ifPresent(worker -> {
                 worker.subscribe(new AggregationSubscriber(strings -> {
-                    strings.stream().filter(s -> !s.isEmpty()).map(string -> {
-                        String[] s = string.split(",");
-                        return new Product(Integer.valueOf(s[0]), s[1], s[2], s[3], Double.valueOf(s[4]));
-                    }).collect(AggregationCollector.toCSVFilesCollector(aggregationBuffer, 20, 1000));
-
+                    strings.stream().filter(s -> !s.isEmpty()).map(string -> Optional.of(string.split(",")).map(s -> new Product(Integer.valueOf(s[0]), s[1], s[2], s[3], Double.valueOf(s[4]))).get()
+                    ).collect(AggregationCollector.toCSVFilesCollector(aggregationBuffer, 20, 1000));
                 }, workers, worker.getFile().toString()));
                 runnedWorkersPool.add(worker);
             }));
-
             //wait until all workers will be completed
             while (!runnedWorkersPool.stream().allMatch(FileWorker::isCompleted)) {
                 Thread.sleep(1000);
